4eb66f89ca7c3761e36a571f2120b6e476255c33
Isaac Sánchez <isaacsancheza@outlook.com>
2023-06-06 23:38:30 +0000

    Update template

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: false,
+  plugins: [
+    require('prettier-plugin-tailwindcss')
+  ],
+  arrowParens: 'avoid',
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   content: [
     './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/templates/**/*.{js,jsx,ts,tsx}',
     './src/components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  mode: 'jit',
 }
