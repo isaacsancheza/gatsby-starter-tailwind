dac6ad02f4aae3b6741088fafef23f7ecfb612a7
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2023-07-01 18:21:03 -0600

    Update seo.jsx

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -1,13 +1,19 @@
 import * as React from 'react'
 
-export default function Seo({ title, children, description }) {
+export default function Seo({ title, pagePath, children, description }) {
+  const url = data.site.siteMetadata.siteUrl + pagePath
   return (
     <>
-      <title>{ title }</title>
+      <title>
+        {title }
+      </title>
+      
       <meta name="description" content={ description } />
-      <meta property="og:title" content={ title } />
-      <meta property="og:description" content={ description } />
+      
+      <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       {children}
     </>
   )
