bf1a7532bc806246ccb9970edafcc3d87c313dc6
Isaac Sánchez <1031819+isaacsancheza@users.noreply.github.com>
2023-09-16 15:18:02 -0600

    Update gatsby-browser.jsx

--- gatsby-browser.jsx
+++ gatsby-browser.jsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 
 import Layout from './src/components/layout'
 
-export const wrapPageElement = ({ element, props }) => {
+export const wrapPageElement = ({element, props}) => {
   return (
     <Layout {...props}>
         {element}
